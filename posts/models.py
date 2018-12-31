ef8f21a82d5a4c9f2a6757c9e4e83819adfa3236
mandeepmourya007 <30563736+mandeepmourya007@users.noreply.github.com>
2018-12-31 20:25:42 +0530

    index link  now we can also add post

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,9 @@ from django.db import models
 
 class posts(models.Model):
     title = models.CharField(max_length=200,blank=True,null=True)
-    slug=models.SlugField(unique=True)
+    tag=models.CharField(max_length=100,blank=True,null=True)
+   # slug=models.SlugField(unique=True)
+
     text = models.TextField(blank=True,null=True)
     image=models.FileField(upload_to="static/images/post",blank=True,null=True)
     def __str__(self):
