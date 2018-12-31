823b83e851929a2d1a005f0e53f00855a10ea214
mandeepmourya007 <30563736+mandeepmourya007@users.noreply.github.com>
2018-12-31 20:44:26 +0530

    Revert "index link  now we can also add post"
    This reverts commit ef8f21a82d5a4c9f2a6757c9e4e83819adfa3236.

--- posts/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class postForm(forms.Form):
-    title = forms.EmailField(required=True)
-    tag = forms.CharField(required=True)
-    text = forms.CharField(widget=forms.Textarea)
--- posts/models.py
+++ posts/models.py
@@ -4,9 +4,7 @@ from django.db import models
 
 class posts(models.Model):
     title = models.CharField(max_length=200,blank=True,null=True)
-    tag=models.CharField(max_length=100,blank=True,null=True)
-   # slug=models.SlugField(unique=True)
-
+    slug=models.SlugField(unique=True)
     text = models.TextField(blank=True,null=True)
     image=models.FileField(upload_to="static/images/post",blank=True,null=True)
     def __str__(self):
